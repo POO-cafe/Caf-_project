83990ec5d1d40946df01c1c6887d9d98ed66dc2d
Megane SMAILI <megane.smaili.dev@gmail.com>
2022-05-18 16:21:36 +0200

    new add repository

--- src/Repository/CoffeRepository.php
+++ src/Repository/CoffeRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Database.php';
+require_once __DIR__ . '../../Entity/Coffe.php';
 
 class CoffeRepository extends Database {
 
@@ -8,6 +9,23 @@ class CoffeRepository extends Database {
     {
         $query = $this->getDb()->query('SELECT * FROM cafes');
         $allCoffees = $query->fetchAll();
+
+        foreach($allCoffees as $avis){
+
+            $avisObject = new Coffe();
+            $avisObject->setId($avis['id']);
+            $avisObject->setNom($avis['nom']);
+            $avisObject->setType($avis['setType']);
+            $avisObject->setPays($avis['pays']);
+            $avisObject->setPhotos($avis['photos']);
+            $avisObject->setProfil_aromatique($avis['profil_aromatique']);
+
+            
+            $objects[] = $avisObject;
+
+        }
+
+        return $objects ?? [];
         
     }
 }
