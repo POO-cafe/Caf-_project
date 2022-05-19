ea56fa56056b1fa615af951bf71fb9de6c298097
Fontaine Gaetan <fontaine_gaetan@orange.fr>
2022-05-19 11:32:29 +0200

    add en cours de MAJ

--- src/Controller/coffeController.php
+++ src/Controller/coffeController.php
@@ -3,16 +3,59 @@
 require_once __DIR__.'../../Repository/CoffeRepository.php';
 require_once __DIR__.'../../Entity/coffe.php';
 
+
+// Selectionne tous les cafés
 class CoffeController {
 
     public function selectType() {
 
-        $coffeRepository = new CoffeRepository();
-        $listAllCoffe = $coffeRepository->selectAll();
-       require_once __DIR__.'../../../templates/index.php';
-   
+            
+            $coffeRepository = new CoffeRepository();
+            $listAllCoffe = $coffeRepository->selectAll();
+            require_once __DIR__.'../../../templates/index.php';
+    
     }
 
+// Insertion d'un café avec condition pour le type du café 'arabica ou robusta'
+    public function insertCoffe() {
+        
+            if (!empty($_POST)) {
+
+
+            $entity = new Entity\Coffe();
+            $entity->setContent(htmlspecialchars(strip_tags($_POST['cafes'])));
+            $coffe->setProfil_aromatique(htmlspecialchars(strip_tags($_POST['id'])));
+            $coffe->setNom(htmlspecialchars(strip_tags($_POST['nom'])));
+            $coffe->setType(htmlspecialchars(strip_tags($_POST['type'])));
+            $coffe->setPays(htmlspecialchars(strip_tags($_POST['pays'])));
+            $coffe->setPhotos(htmlspecialchars(strip_tags($_POST['photos'])));
+            $coffe->setPrix(htmlspecialchars(strip_tags($_POST['prix'])));
+
+
+            // Insertion dans la BDD
+            $coffeRepository = new CoffeRepository();
+            $success = $coffeRepository->add($entity);
+            
+            if (
+                !empty($title) 
+                && !empty($content) 
+                && !empty($category) 
+                && !empty($_FILES['cover']) 
+                && $_FILES['cover']['error'] === 0
+            )
+
+            if (($_POST['type'] != 'arabica' || $_POST['type']'robusta')) {
+                
+                return false
+            }
+        }
+
+        require_once __DIR__.'../../../templates/index.php';
+    }
+
+//---------------------------------CAFES ROBUSTA----------------------------------------------------
+    
+    //Selectionne uniquement les cafés robusta
     public function selectAllRobusta() {
         
         $coffeRepository = new CoffeRepository();
@@ -20,34 +63,39 @@ class CoffeController {
        require_once __DIR__.'../../../templates/robusta.php'
         
     }
-
+    //Modification des produits robusta
     public function editRobusta() {
         
         $coffeRepository = new CoffeRepository();
         $robusta = $coffeRepository->edit($_GET['id']);
 
         if (!empty($_POST)) {
-            $robusta->setContent(htmlspecialchars(strip_tags($_POST['cafes'])));
-           //Transmet  cet objet à une methode du repository pour mise à jour
-           $success = $coffeRepository->editRobusta($robusta);
+            $robusta->setNom(htmlspecialchars(strip_tags($_POST['nom'])));
+            $robusta->setType(htmlspecialchars(strip_tags($_POST['type'])));
+            $robusta->setPays(htmlspecialchars(strip_tags($_POST['pays'])));
+            $robusta->setPhotos(htmlspecialchars(strip_tags($_POST['photos'])));
+            $robusta->setPrix(htmlspecialchars(strip_tags($_POST['prix'])));
+            $robusta->setProfil_aromatique($coffee['profil_aromatique']);
+
+           
+            $success = $coffeRepository->editRobusta($robusta);
        }
-        // Redirige l'utilisateur vers la route "/liste"
-       // header('Location: /liste?edit='. $update);
+        
        require_once __DIR__.'../../../templates/robusta.php';
    }
 
-
+   //Suppression des café robusta
     public function deleteRobusta() {
    
-        $coffeRepository = new CoffeRepositor();
+         $coffeRepository = new CoffeRepositor();
          $success = $coffeRepository->remove($_GET['id']);
         
-         // Redirige l'utilisateur vers la route "/liste"
-        header('Location: /liste?delete='. $success);
+        
+         header('Location: /arabica?delete='. $success);
     }
+//---------------------------------CAFES ARABICA-----------------------------------------------------
 
-
-
+    // Selectionne tous les cafés arabica
     public function selectAllArabica() {
    
         $coffeRepository = new CoffeRepository();
@@ -55,19 +103,25 @@ class CoffeController {
        require_once __DIR__.'../../../templates/arabica.php'
     }
 
+    //Modification des produits arabica
     public function editArabica() {
    
         $coffeRepository = new CoffeRepository();
-        $arabica = $coffeRepository->edit($_GET['id']);
+        $arabica->setNom(htmlspecialchars(strip_tags($_POST['nom'])));
+        $arabica->setType(htmlspecialchars(strip_tags($_POST['type'])));
+        $arabica->setPays(htmlspecialchars(strip_tags($_POST['pays'])));
+        $arabica->setPhotos(htmlspecialchars(strip_tags($_POST['photos'])));
+        $arabica->setPrix(htmlspecialchars(strip_tags($_POST['prix'])));
+        $arabica = $coffeRepository->editArabica($_GET['id']);
+        $arabica->setProfil_aromatique($coffee['profil_aromatique']);
 
         if (!empty($_POST)) {
             $arabica->setContent(htmlspecialchars(strip_tags($_POST['cafes'])));
-           //Transmet  cet objet à une methode du repository pour mise à jour
+           
            $success = $coffeRepository->editArabica($arabica);
        }
-        // Redirige l'utilisateur vers la route "/liste"
-       // header('Location: /liste?edit='. $update);
-       require_once __DIR__.'../../../templates/robusta.php';
+        
+       require_once __DIR__.'../../../templates/arabica.php';
     }
 
 
