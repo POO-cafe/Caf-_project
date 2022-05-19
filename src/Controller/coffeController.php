9b239ae80df1fbc450761d95018b955bb5be4c74
Fontaine Gaetan <fontaine_gaetan@orange.fr>
2022-05-19 13:33:52 +0200

    maj 13heures

--- src/Controller/coffeController.php
+++ src/Controller/coffeController.php
@@ -81,7 +81,7 @@ class CoffeController {
 
    //Suppression des café robusta
     public function deleteRobusta() {
-   
+     
          $coffeRepository = new CoffeRepository();
          $success = $coffeRepository->remove($_GET['id']);
         
