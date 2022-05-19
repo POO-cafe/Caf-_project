9ccd477d272a39b97032c6e98700d9b08247a4cd
Megane SMAILI <megane.smaili.dev@gmail.com>
2022-05-19 09:53:20 +0200

    repository fini

--- src/Repository/CoffeRepository.php
+++ src/Repository/CoffeRepository.php
@@ -10,15 +10,15 @@ class CoffeRepository extends Database {
         $query = $this->getDb()->query('SELECT * FROM cafes');
         $allCoffees = $query->fetchAll();
 
-        foreach($allCoffees as $avis){
+        foreach($allCoffees as $coffee){
 
             $avisObject = new Coffe();
-            $avisObject->setId($avis['id']);
-            $avisObject->setNom($avis['nom']);
-            $avisObject->setType($avis['setType']);
-            $avisObject->setPays($avis['pays']);
-            $avisObject->setPhotos($avis['photos']);
-            $avisObject->setProfil_aromatique($avis['profil_aromatique']);
+            $avisObject->setId($coffee['id']);
+            $avisObject->setNom($coffee['nom']);
+            $avisObject->setType($coffee['type']);
+            $avisObject->setPays($coffee['pays']);
+            $avisObject->setPhotos($coffee['photos']);
+            $avisObject->setProfil_aromatique($coffee['profil_aromatique']);
 
             
             $objects[] = $avisObject;
@@ -27,5 +27,105 @@ class CoffeRepository extends Database {
 
         return $objects ?? [];
         
+    }
+
+    public function selectAllArabica ()
+    {
+        $query = $this->getDb()->query('SELECT * FROM cafes WHERE type = "arabica"');
+        $allCoffees = $query->fetchAll();
+
+        foreach($allCoffees as $coffee){
+
+            $avisObject = new Coffe();
+            $avisObject->setId($coffee['id']);
+            $avisObject->setNom($coffee['nom']);
+            $avisObject->setType($coffee['type']);
+            $avisObject->setPays($coffee['pays']);
+            $avisObject->setPhotos($coffee['photos']);
+            $avisObject->setProfil_aromatique($coffee['profil_aromatique']);
+
+            
+            $objects[] = $avisObject;
+
+        }
+
+        return $objects ?? [];
+        
+    }
+    public function selectAllRobusta ()
+    {
+        $query = $this->getDb()->query('SELECT * FROM cafes WHERE type = "robusta"');
+        $allCoffees = $query->fetchAll();
+
+        foreach($allCoffees as $coffee){
+
+            $avisObject = new Coffe();
+            $avisObject->setId($coffee['id']);
+            $avisObject->setNom($coffee['nom']);
+            $avisObject->setType($coffee['type']);
+            $avisObject->setPays($coffee['pays']);
+            $avisObject->setPhotos($coffee['photos']);
+            $avisObject->setProfil_aromatique($coffee['profil_aromatique']);
+
+            
+            $objects[] = $avisObject;
+
+        }
+
+        return $objects ?? [];
+        
+    }
+    public function add(Coffe $coffee)
+    {
+        $query = $this->getDb()->prepare('INSERT INTO cafes (nom, type, pays, photos, profil_aromatique) VALUES (:nom, :type, :pays, :photos, :profil_aromatique)');
+        $query->bindValue(':nom', $coffee->getNom());
+        $query->bindValue(':type',$coffee->getType());
+        $query->bindValue(':pays',$coffee->getPays());
+        $query->bindValue(':photos',$coffee->getPhotos());
+        $query->bindValue('profil_aromatique',$coffee->getProfil_aromatique());
+
+        return $query->execute();
+    }
+
+    public function remove(int $id)
+    {
+        
+        $query = $this->getDb()->prepare('DELETE FROM cafes WHERE id = :id');
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        return $query->execute();
+    }
+
+    public function edit(int $id)
+    {
+        
+        $query = $this->getDb()->prepare('SELECT * FROM cafes WHERE id = :id');
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+       
+        $coffee = $query->fetch();
+
+        if ($coffee) {
+            $avisObject = (new Coffe())
+            ->setId($coffee['id'])
+            ->setNom($coffee['nom'])
+            ->setType($coffee['type'])
+            ->setPays($coffee['pays'])
+            ->setPhotos($coffee['photos'])
+            ->setProfil_aromatique($coffee['profil_aromatique']);
+        }
+        return $avisObject ?? false;
+       
+    }
+    public function update(Coffe $coffee)
+    {
+        
+        $query = $this->getDb()->prepare('UPDATE cafes SET nom = :nom, type = :type, pays = :pays, photos = :photos, profil_aromatique = :profil_aromatique WHERE id = :id');
+        $query->bindValue(':id', $coffee->getId(), PDO::PARAM_INT);
+        $query->bindValue(':nom', $coffee->getNom());
+        $query->bindValue(':type',$coffee->getType());
+        $query->bindValue(':pays',$coffee->getPays());
+        $query->bindValue(':photos',$coffee->getPhotos());
+        $query->bindValue('profil_aromatique',$coffee->getProfil_aromatique());
+        return $query->execute();
     }
 }
