d42868b5ab5183cae5a693197faa8df9512f7cde
Fontaine Gaetan <fontaine_gaetan@orange.fr>
2022-05-20 11:01:24 +0200

    maj

--- templates/layouts/footer.php
+++ templates/layouts/footer.php
@@ -1,3 +1,3 @@
-<div class="p-2 bg-secondary text-white position-fixed bottom-0 left-0 right-0 w-100">
+<div class="p-2  position-fixed bottom-0 left-0 right-0 w-100" id="footer">
     <p class="p-1 m-0 text-center">&copy; Copyright Coffee Break. 2022</p>
 </div>
--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,21 +1,21 @@
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-lg bg-light"  >
+  <div class="container-fluid header">
 
 
-        <a href="/" class="navbar-brand "><img src="images/logo.png" class="img_header " alt="logo">Coffee Break.</a>
+        <a href="/" class="navbar-brand "><img src="images/logo.png" class="img_header " alt="logo" >Coffee Break.</a>
 
 
 
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <ul class="nav navbar-nav"  >
+            <li class="nav-item textheader">
+                <a class="nav-link active " aria-current="page" href="/">Home</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/arabica">Arabica</a>
+            <li class="nav-item textheader">
+                <a class="nav-link active " aria-current="page" href="/arabica">Arabica</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/robusta">Robusta</a>
+            <li class="nav-item textheader">
+                <a class="nav-link active " aria-current="page" href="/robusta">Robusta</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link active btn btn-dark text-white" aria-current="page" href="/add">+ Add.</a>
